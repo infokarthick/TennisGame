be49825a815c559dc9682f8f123468f03f478009
kataUser <dummyemail@gmail.com>
2019-07-03 20:59:08 +0200

    Initial project setup

---
+++ src/test/java/com/channels/kata/TennisGameTest.java
@@ -0,0 +1,15 @@
+package com.channels.kata;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TennisGameTest {
+
+    @Test
+    public void setUpTest(){
+        assertTrue(true);
+    }
+
+
+}
