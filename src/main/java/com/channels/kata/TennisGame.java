a6e05d53fd8cc66daaed4ed5bd881bf6a13310b9
kataUser <dummyemail@gmail.com>
2019-07-04 20:30:10 +0200

    If at least three points have been scored by each side and a player has one more point than his opponent, the score of the game is “advantage” for the player in the lead.

--- src/main/java/com/channels/kata/TennisGame.java
+++ src/main/java/com/channels/kata/TennisGame.java
@@ -4,16 +4,16 @@ import com.channels.kata.model.Player;
 
 public class TennisGame {
 
-    private static final String[] score = {"Love", "Fifteen", "Thirty", "Forty"};
-
-    public static final int MINIMUM_WINNING_SCORE = 4;
-    public static final int WINNING_DIFFERENCE_POINT = 2;
-    public static final String WON_THE_MATCH = " won the match";
+    private static final int MINIMUM_WINNING_SCORE = 4;
+    private static final int ADVANTAGE_DIFFERENCE_POINT = 1;
+    private static final int MINIMUM_DEUCE_POINT = 3;
 
     private static final char HYPHEN = '-';
-    private static final int MINIMUM_DEUCE_POINT = 3;
+    private static final String WON_THE_MATCH = " won the match";
     private static final String HYPHEN_ALL = HYPHEN + "All";
     private static final String DEUCE = "Deuce";
+
+    private static final String[] score = {"Love", "Fifteen", "Thirty", "Forty"};
 
     private Player player1;
     private Player player2;
@@ -44,16 +44,20 @@ public class TennisGame {
             } else {
                 gameScore = score[player1.getPoint()] + HYPHEN_ALL;
             }
-        } else if (isAnyPlayerEligibleToWin() && isPointsDifferenceGreaterThanOrEqualToTwo()) {
-            gameScore = getTopScorerName() + WON_THE_MATCH;
+        } else if (isAnyPlayerEligibleToWin()) {
+            if (isPointsDifferenceEqualToOne()) {
+                gameScore = getTopScorerName() + " has advantage";
+            } else {
+                gameScore = getTopScorerName() + WON_THE_MATCH;
+            }
         } else {
             gameScore = score[player1.getPoint()] + HYPHEN + score[player2.getPoint()];
         }
         return gameScore;
     }
 
-    private boolean isPointsDifferenceGreaterThanOrEqualToTwo() {
-        return Math.abs(player1.getPoint() - player2.getPoint()) >= WINNING_DIFFERENCE_POINT;
+    private boolean isPointsDifferenceEqualToOne() {
+        return Math.abs(player1.getPoint() - player2.getPoint()) == ADVANTAGE_DIFFERENCE_POINT;
     }
 
     private boolean isAnyPlayerEligibleToWin() {
--- src/test/java/com/channels/kata/TennisGameTest.java
+++ src/test/java/com/channels/kata/TennisGameTest.java
@@ -73,8 +73,10 @@ public class TennisGameTest {
     }
 
     @ParameterizedTest
-    @CsvSource({"1,0,Fifteen-Love", "0,1,Love-Fifteen", "1,1,Fifteen-All", "1,2,Fifteen-Thirty", "2,1,Thirty-Fifteen", "1,3,Fifteen-Forty", "2,3,Thirty-Forty", "3,2,Forty-Thirty", "2,2,Thirty-All"})
-    @DisplayName("The running score of each game is described in a manner peculiar to tennis: scores from zero to three points are described as “love”, “fifteen”, “thirty”, and “forty” respectively")
+    @CsvSource({"1,0,Fifteen-Love", "0,1,Love-Fifteen", "1,1,Fifteen-All", "1,2,Fifteen-Thirty",
+            "2,1,Thirty-Fifteen", "1,3,Fifteen-Forty", "2,3,Thirty-Forty", "3,2,Forty-Thirty", "2,2,Thirty-All"})
+    @DisplayName("The running score of each game is described in a manner peculiar to tennis:" +
+            " scores from zero to three points are described as “love”, “fifteen”, “thirty”, and “forty” respectively")
     public void runningScoreShouldBeDescribedInAMannerPeculiarToTennis(int player1Points, int player2Points, String gameScore) {
         serviceWinByPlayer(tennisGame.getPlayer1(), player1Points);
         serviceWinByPlayer(tennisGame.getPlayer2(), player2Points);
@@ -94,9 +96,21 @@ public class TennisGameTest {
 
 
     @ParameterizedTest
-    @CsvSource({"4,0,Player1 won the match", "4,2,Player1 won the match", "6,4,Player1 won the match", "10,8,Player1 won the match", "0,4,Player2 won the match", "1,4,Player2 won the match", "3,5,Player2 won the match", "6,8,Player2 won the match", "9,11,Player2 won the match"})
+    @CsvSource({"4,0,Player1 won the match", "4,2,Player1 won the match", "6,4,Player1 won the match",
+            "10,8,Player1 won the match", "0,4,Player2 won the match", "1,4,Player2 won the match",
+            "3,5,Player2 won the match", "6,8,Player2 won the match", "9,11,Player2 won the match"})
     @DisplayName("A game is Won by a player to have won at least four points in total and at least two points more than the opponent")
     public void gameIsWonByPlayerToHaveMinimumFourPointsInTotalAndMinimumTwoPointsMoreThanTheOpponent(int player1Points, int player2Points, String gameScore) {
+        serviceWinByPlayer(tennisGame.getPlayer1(), player1Points);
+        serviceWinByPlayer(tennisGame.getPlayer2(), player2Points);
+
+        assertEquals(gameScore, tennisGame.getGameScore());
+    }
+
+    @ParameterizedTest
+    @CsvSource({"4,3,Player1 has advantage"})
+    @DisplayName("If at least three points have been scored by each side and a player has one more point than his opponent, the score of the game is “advantage” for the player in the lead.")
+    public void whenAtleastThreePointsScoredByEachPlayerAndPointDifferenceIsOneThenGameIsAdvantageForThePlayerInLead(int player1Points, int player2Points, String gameScore) {
         serviceWinByPlayer(tennisGame.getPlayer1(), player1Points);
         serviceWinByPlayer(tennisGame.getPlayer2(), player2Points);
 
